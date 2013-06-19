c8b7802dbdd7a28fe504dbf856c735dd8448845a
Hack Reactor Students <github@hackreactor.com>
2013-06-19 14:58:33 -0700

    implemented stack for the first time

--- spec/stackSpec.js
+++ spec/stackSpec.js
@@ -23,18 +23,34 @@ describe("stack", function() {
 
   it('should add pushed items to the top of the stack', function() {
     // Fill out the body of the test here
+    // add 1 to size
+    // add the new item to the stack
+    stack.push("test");
+    expect(stack.size()).to.equal(1);
+    stack.push("hi");
+    expect(stack.getTop()).to.equal("hi");
   });
 
   it('should remove popped items from the top of the stack', function() {
     // Fill out the body of the test here
+    stack.push("test");
+    stack.push("hi");
+    stack.pop();
+    expect(stack.getTop()).to.equal("test");
   });
 
   it('should push and pop multiple items in the right order (LIFO)', function() {
     // Fill out the body of the test here
+    // stack.push("test", "hi") ==> storage {0: 'test', 1: 'hi'}
+    stack.push("test", "hi", 5);
+    expect(stack.getTop()).to.equal(5);
+    stack.pop(2);
+    expect(stack.getTop()).to.equal("test");
   });
 
   it('should not error when popping from an empty stack', function() {
     // Fill out the body of the test here
+    expect(stack.pop()).to.equal(0);
   });
 
   // Hey! Add more tests here if you can think of ways to test your stack more thoroughly
--- src/functional/stack.js
+++ src/functional/stack.js
@@ -2,19 +2,45 @@ var makeStack = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
   var stack = {};
 
   stack.push = function(value){
+    //console.log(arguments);
+    for(var i = 0; i< arguments.length; i++){
+      storage[size] = arguments[i];
+      size += 1;
+    }
   };
 
-  stack.pop = function(){
+  stack.pop = function(noOfItems){
+    if (size > 0) {
+      noOfItems = noOfItems || 1;
+      var returnValue =[];
+      for(var i = 0; i< noOfItems; i++){
+        returnValue.push(storage[size - 1]);
+        delete storage[size - 1];
+        size -= 1;
+      }
+      return returnValue;
+    } else {
+      return 0;
+    }
   };
 
   stack.size = function(){
+    return size;
   };
 
+  stack.display = function() {
+    for (var key in storage) {
+      console.log(key + ": " + storage[key]);
+    }
+  };
+  stack.getTop = function(){
+    return storage[size-1];
+  };
   return stack;
 };
